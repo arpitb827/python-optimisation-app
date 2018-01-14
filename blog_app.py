6b790e1b9b1b8bb10c2168ec6b91237391d08831
arpit <arpitb827@gmail.com>
2018-01-14 15:46:30 +0530

    adding new files

--- blog_app.py
+++ blog_app.py
@@ -10,5 +10,13 @@ def index():
 @app.route("/sign_in")
 def open_login():
 	return render_template("login_activity.html")
+
+@app.route("/create_account")
+def create_login():	
+	return render_template("create_account.html")
+
+@app.route("/password_reset")
+def forgot_password():
+	return render_template("forgot_password.html")
 if __name__ == '__main__':
    app.run(debug=True)
